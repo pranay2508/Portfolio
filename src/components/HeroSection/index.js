ab8122ddf1cedbb3dd3baf56196577a6d7c87d34
pranay2508 <bhatnagarpranay8@gmail.com>
2024-01-20 18:01:03 +0530

    Navbar

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,238 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { Bio } from "../../data/constants";
+import Typewriter from "typewriter-effect";
+import passport_size_photo from "../../images/passport_size_photo.jpeg";
+import HeroBgAnimation from "../../HeroBgAnimation";
 
+const HeroContainer = styled.div`
+  background-color: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+
+  @media screen and (max-width: 960px) {
+    padding: 66px 16px;
+  }
+  @media screen and (max-width: 480px) {
+    padding: 32px 16px;
+  }
+  z-index: 1;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HeroBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 50%;
+  right: 0;
+  bottom: 0;
+  left: 50%;
+  overflow: hidden;
+  width: 82%;
+  height: 100%;
+  padding: 0 30px;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  @media (max-width: 960px) {
+    justify-content: center;
+    padding: 0 0px;
+  }
+`;
+
+const HeroInnerContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HeroLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media screen and (max-width: 960px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  @media screen and (max-width: 640px) {
+    order: 2;
+    margin-bottom: 32px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+const HeroRightContainer = styled.div`
+  width: 100%;
+  order: 2;
+  display: flex;
+  justify-content: end;
+  gap: 12px;
+  @media screen and (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    margin-bottom: 80px;
+    align-items: center;
+  }
+  @media screen and (max-width: 640px) {
+    order: 1;
+    margin-bottom: 30px;
+  }
+`;
+
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+  @media screen and (max-width: 640px) {
+    font-size: 40px;
+    line-height: 48px;
+    margin-bottom: 8px;
+  }
+`;
+
+const TextLoop = styled.div`
+  font-size: 32px;
+  font-weight: 600px;
+  color: ${({ theme }) => theme.text_primary};
+  line-height: 68px;
+  gap: 12px;
+  display: flex;
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+  @media screen and (max-width: 640px) {
+    font-size: 22px;
+    line-height: 48px;
+    margin-bottom: 16px;
+  }
+`;
+
+const Span = styled.div`
+  color: ${({ theme }) => theme.primary};
+  cursor: pointer;
+`;
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  color: ${({ theme }) => theme.text_primary + 95};
+  line-height: 32px;
+  margin-bottom: 42px;
+  @media screen and (max-width: 960px) {
+    text-align: center;
+  }
+  @media screen and (max-width: 640px) {
+    font-size: 16px;
+    line-height: 32px;
+  }
+`;
+
+const ResumeButton= styled.div`
+-webkit-appearance: button;
+-moz-appearance: button;
+appearance: button;
+text-decoration: none;
+width: 95%;
+max-width: 300px;
+text-align: center;
+padding: 16px 0;
+color:${({ theme }) => theme.white};
+border-radius: 20px;
+cursor: pointer;
+font-size: 20px;
+font-weight: 600;
+transition: all 0.2s ease-in-out !important;
+background: hsla(271, 100%, 50%, 1);
+background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+box-shadow:  20px 20px 60px #1F2634,
+-20px -20px 60px #1F2634;
+&:hover {
+  transform: scale(1.05);
+  transition: all 0.4s ease-in-out;
+  box-shadow: 20px 20px 60px #1F2634;
+
+  @media (max-width: 640px) {
+    padding: 12px 0;
+    font-size: 18px;
+  }
+}   
+`;
+
+const Image=styled.img`
+position: relative;
+width: 100%;
+height: 100%;
+max-width: 400px;
+max-height: 400px;
+border-radius: 50%;
+border: 2px solid ${({ theme }) => theme.primary};
+
+@media (max-width: 768px) {
+  max-width: 400px;
+  max-height: 400px;
+}
+
+@media (max-width: 640px) {
+  max-width: 280px;
+  max-height: 280px;
+}
+`;
+
+
+//---------------------------------------------------------------------------------------------------------
 const Hero = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div id="about">
+      <HeroContainer>
+        <HeroBg>
+          <HeroBgAnimation/>
+        </HeroBg>
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title>
+              Hi, I am <br />
+              {Bio.name}
+            </Title>
+            <TextLoop>
+              I am a
+              <Span>
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Span>
+            </TextLoop>
+            <SubTitle>{Bio.description}</SubTitle>
+            <ResumeButton href={Bio.resume} target="display">
+              Check Resume{" "}
+            </ResumeButton>
+          </HeroLeftContainer>
+          <HeroRightContainer>
+            <Image src={passport_size_photo} alt="hero"/>
+          </HeroRightContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
 
 export default Hero;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { Link as LinkR } from "react-router-dom";
-
+import { DiCssdeck } from "react-icons/di";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 const Nav = styled.div`
   background-color: ${({ theme }) => theme.card_light};
@@ -116,19 +118,136 @@ const ButtonContainer = styled.div`
   }
 `;
 
+export const Span = styled.div`
+  padding: 0 4px;
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const MobileMenu = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+  position: absolute;
+  top: 80;
+  right: 0;
+  width: 100%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.card_light + 99};
+  transition: all 0.3s ease-in-out;
+  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%")};
+  border-radius: 0 0 20 20px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0.3);
+  opacity: ${({ open }) => (open ? "1" : "0")};
+  z-index: ${({ open }) => (open ? "1" : "-1")};
+`;
+const MobileMenuLinks = styled(LinkR)`
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+
+
 // ---------------------------------------------------- start from here ------------------------------------------
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
   return (
     <Nav>
       <NavContainer>
-        <NavLogo>Logo</NavLogo>
-        <MobileIcon></MobileIcon>
+        <NavLogo to="/">
+          <a
+            style={{
+              display: "flex",
+              alignItems: "center",
+              color: "white",
+              marginBottom: "20;",
+              cursor: "pointer",
+            }}
+          >
+            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars
+            onClick={() => {
+              setOpen(!open);
+            }}
+          />
+        </MobileIcon>
         <NavItems>
-          <NavLink>Home</NavLink>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href="#skills">Skills</NavLink>
+          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#projects">Projects</NavLink>
+          <NavLink href="#education">Education</NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton>Github Profile</GitHubButton>
         </ButtonContainer>
+        {open && (
+          <MobileMenu open={open}>
+            <MobileMenuLinks
+              href="#about"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              About
+            </MobileMenuLinks>
+            <MobileMenuLinks
+              href="#skills"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Skills
+            </MobileMenuLinks>
+            <MobileMenuLinks
+              href="#experience"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Experience
+            </MobileMenuLinks>
+            <MobileMenuLinks
+              href="#projects"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Projects
+            </MobileMenuLinks>
+            <MobileMenuLinks
+              href="#education"
+              onClick={() => {
+                setOpen(!open);
+              }}
+            >
+              Education
+            </MobileMenuLinks>
+            <GitHubButton
+              style={{
+                padding: "10px 16px",
+                background: `${theme.primary}`,
+                color: "white",
+                width: "max-content",
+              }}
+              href="/"
+              target="_blank"
+            >
+              Github Profile
+            </GitHubButton>
+          </MobileMenu>
+        )}
       </NavContainer>
     </Nav>
   );
