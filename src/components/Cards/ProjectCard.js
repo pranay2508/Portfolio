b458587a2e43a79ad90f64808fca4b3e7a26b557
pranay2508 <bhatnagarpranay8@gmail.com>
2024-01-25 21:47:09 +0530

    ProjectCard

--- src/components/Cards/ProjectCard.js
+++ src/components/Cards/ProjectCard.js
@@ -2,56 +2,56 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Card = styled.div`
-width:330px;
-height:490px;
-background-color: ${({theme})=>theme.card};
-cursor: pointer;
-border-radius: 10px;
-box-shadow: 0 0 12px 4px rgba(0,0,0,0.4);
-overflow: hidden;
-transition: all 0.5s ease-in-out;
-padding: 26px 20px ;
-display: flex;
-flex-direction: column;
-gap:14px;
-&:hover { 
-    transform: translateY(-10px);
-    box-shadow: 0 0 12px 4px rgba(0,0,0,0.6);
-    filter:brightness()(1.1);
-}
-`;
+    width: 330px;
+    height: 490px;
+    background-color: ${({ theme }) => theme.card};
+    cursor: pointer;
+    border-radius: 10px;
+    box-shadow: 0 0 12px 4px rgba(0,0,0,0.4);
+    overflow: hidden;
+    padding: 26px 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    transition: all 0.5s ease-in-out;
+    &:hover {
+        transform: translateY(-10px);
+        box-shadow: 0 0 50px 4px rgba(0,0,0,0.6);
+        filter: brightness(1.1);
+    }
+    
+`
 
-const Image= styled.div`
-width: 100%;
-height:180px;
-background-color:${({theme})=>theme.white} ;
-border-radius: 10px;
-object-fit:cover;
-box-shadow:0 0 16px 2px rgba(0,0,0,0.3);
-`;
+const Image = styled.img`
+    width: 100%;
+    height: 180px;
+    background-color: ${({ theme }) => theme.white};
+    border-radius: 10px;
+    box-shadow: 0 0 16px 2px rgba(0,0,0,0.3);
+`
 
 const Tags = styled.div`
-width: 100%;
-display: flex;
-flex-wrap: wrap;
-gap:8px;
-margin-top: 4px;
-align-items: center;
-`;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 8px;
+    margin-top: 4px;
+`
 
 const Details = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-gap:0px;
-padding: 0px 2px;
-`;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0px;
+    padding: 0px 2px;
+`
 
-const Members= styled.div`
-display: flex;
-align-items: center;
-padding-left:10px ;
-`;
+const Members = styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+`
 const ProjectCard = ({project}) => {
   return (
     <Card>
